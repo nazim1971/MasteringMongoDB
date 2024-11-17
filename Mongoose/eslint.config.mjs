b6737f85e6e8d279fe8f191f606b0bd3938b5b28
nazim1971 <md.nazimuddinaj@gmail.com>
2024-11-17 21:25:08 +0600

    update eslint

--- Mongoose/eslint.config.mjs
+++ Mongoose/eslint.config.mjs
@@ -1,26 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
-/** @type {import('eslint').Linter.Config} */
-export default {
-  overrides: [
-    {
-      files: ["**/*.{js,mjs,cjs,ts}"], // Apply to JavaScript & TypeScript files
-      languageOptions: {
-        globals: globals.node, // Use Node.js global variables
-        parser: tsParser,      // Parse TypeScript files with the correct parser
-      },
-      extends: [
-        "eslint:recommended",                  // ESLint recommended base rules
-        "plugin:@typescript-eslint/recommended", // TypeScript recommended rules
-      ],
-      rules: {
-        "no-unused-vars": "error",  // Throw an error for unused variables
-        "@typescript-eslint/no-unused-vars": "error", // TypeScript specific rule for unused variables
-      },
-      ignorePatterns: ["node_modules/", "dist/"], // Ignore these folders to avoid unnecessary linting
-    },
-  ],
-};
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
--- Mongoose/src/app.ts
+++ Mongoose/src/app.ts
@@ -7,6 +7,7 @@ app.use(express.json());
 app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
+    let a = 10;
   res.send('Hello World!')
 })
 
