456cf85720dd05a8f305c5c1a63dcd9efc29b511
nazim1971 <md.nazimuddinaj@gmail.com>
2024-11-19 11:40:11 +0600

    refector prettier

--- Mongoose/src/app/modules/student/student.interface.ts
+++ Mongoose/src/app/modules/student/student.interface.ts
@@ -1,17 +1,40 @@
 import { Schema, model, connect } from 'mongoose';
 
 // 1. Create an interface representing a document in MongoDB.
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type LocalGuardian = {
+  Name: string;
+  Occupation: string;
+  ContactNo: string;
+};
+
 export type Student = {
   id: string;
-  name: {
-    firstName: string;
-    middleName: string;
-    lastName: string;
-  },
-  gender: "male" | "female";
+  name: UserName;
+  gender: 'male' | 'female';
   dateOfBirth: string;
   email: string;
   contactNo: string;
   emergencyContactNo: string;
-  bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
-}
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImage?: string;
+  isActive: 'active' | 'inActive';
+};
